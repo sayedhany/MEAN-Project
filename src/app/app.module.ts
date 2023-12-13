4bd74986a5948f4066be82b3ad61b31fe3e3de6e
sayedhany <shany@kn-it.com>
2023-12-13 09:44:07 +0200

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-
+import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { mat } from './mat';
+import { HeaderComponent } from './header/header.component';
+import { FormsModule } from '@angular/forms';
+import { PostListComponent } from './posts/post-list/post-list.component';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, PostCreateComponent, HeaderComponent, PostListComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, FormsModule, ...mat],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/mat.ts
@@ -0,0 +1,16 @@
+import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatInputModule } from '@angular/material/input';
+import { MatExpansionModule } from '@angular/material/expansion';
+
+export const mat = [
+  FormsModule,
+  MatCardModule,
+  MatButtonModule,
+  MatToolbarModule,
+  MatInputModule,
+  MatExpansionModule,
+];
